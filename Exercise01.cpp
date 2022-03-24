2c997c43a9498f97c5f6f606c02b4913361b6d4d
it21323584 <99316637+it21323584@users.noreply.github.com>
2022-03-24 16:56:55 +0000

    completed

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -5,8 +5,11 @@ int main() {
   
 // == DO NOT CHANGE THE MAIN PROGRAM =================
    Student mystd;
+  
    mystd.assignDetails(1212, "Amalinga");
+  
    mystd.display();
 //==========================
+  
    return 0;
 }
